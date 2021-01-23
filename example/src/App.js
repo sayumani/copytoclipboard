6dbfd5577cc929c26afae32783b5de50f0e11d5a
Sayooj V R <sayoojvylappully@gmail.com>
2021-01-23 11:33:01 +0530

    Initial implementation

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'copytoclipboard'
+import CopyToCLipBoard from 'copytoclipboard'
 import 'copytoclipboard/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <CopyToCLipBoard text='Create React Library Example 😄' />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+class CopyToCLipBoard extends React.Component {
+  constructor() {
+    super()
+  }
+
+  copyToClipBoard = async (copyMe) => {}
+
+  onClick = async (event) => {
+    const { innerText } = event.target
+    console.log(event.target.innerText)
+    try {
+      await navigator.clipboard.writeText(innerText)
+      alert('Copied!')
+    } catch (err) {
+      alert('Failed to copy!')
+    }
+  }
+
+  render() {
+    const defaultText = 'please provide text'
+    let { height, width, text } = this.props
+    height = height ? text : '40px'
+    width = width ? width : 'fit-content'
+    return (
+      <div
+        className={styles.test}
+        style={{ height: height, width: width }}
+        onClick={this.onClick}
+      >
+        {text ? text : defaultText}
+        <span className={styles.copy_icon}>
+          <FontAwesomeIcon icon={faCopy} />
+        </span>
+      </div>
+    )
+  }
 }
+
+export default CopyToCLipBoard
